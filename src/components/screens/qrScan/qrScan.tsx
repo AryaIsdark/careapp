7d3af73aeaeac66d755159de01ba0eb6081dd7cd
AryaShahali <ars@bankingcircle.com>
2020-02-10 22:59:40 +0100

    flex-wrap

--- src/components/screens/qrScan/qrScan.tsx
+++ src/components/screens/qrScan/qrScan.tsx
@@ -2,52 +2,52 @@ import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import QrReader from 'react-qr-reader'
 import * as api from 'api/apiFunctions'
-import { Alert, Divider } from 'antd';
+import { Alert } from 'antd';
 import { Link } from 'react-router-dom';
 
 const QRScan = () => {
     const { t } = useTranslation();
     const [hasScannedData, setHasScannedData] = useState(false)
     const [isScanning, setIsScanning] = useState(false)
-    const [isValid, setIsValid] = useState(false) 
-    const [showValidationError, setShowValidationError]= useState(false)
+    const [isValid, setIsValid] = useState(false)
+    const [showValidationError, setShowValidationError] = useState(false)
     const resetValidation = () => {
         setIsValid(false)
         setShowValidationError(false)
     }
     const handleError = (error: any) => console.log(error)
-    const handleScan = async(data: any) => {
+    const handleScan = async (data: any) => {
         console.log(data)
-        if(data !== null){
+        if (data !== null) {
             setHasScannedData(true)
-            try{
-                const response : any = await api.canPurchase(data)
-                if(response && response.data === true) {
+            try {
+                const response: any = await api.canPurchase(data)
+                if (response && response.data === true) {
                     setIsValid(true)
                 }
-                else{
+                else {
                     setShowValidationError(true)
                 }
             }
-            catch(error) {
+            catch (error) {
                 alert('something went wrong')
             }
         }
     }
 
     return (
-       <div className={'qr-content'}>
-       {isValid && <><Alert type={'success'} message={'Proccess was succesful'}></Alert><Divider/></>}
-       {showValidationError && <><Alert type={'error'} message={'User has no valid ticket'}></Alert><Divider/></>}
-        
-        <QrReader
-          delay={300}
-          onError={handleError}
-          facingMode={'environment'}
-          onScan={handleScan}
-          style={{ width: '100%' }}
-        />
-       </div>
+        <>
+            <QrReader
+                delay={300}
+                onError={handleError}
+                facingMode={'environment'}
+                onScan={handleScan}
+                style={{ width: '100%' }}
+            />
+            {isValid && <Alert type={'success'} message={'Proccess was succesful'}></Alert>}
+            {showValidationError && <Alert type={'error'} message={'User has no valid ticket'}></Alert>}
+
+        </>
     )
 }
 
